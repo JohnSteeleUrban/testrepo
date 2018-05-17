1e98c65ceee096d18b0b652f659714260f0e57c0
JohnSteeleUrban <John.Urban@learnondemandsystems.com>
2018-05-17 18:29:32 -0400

    file

---
+++ Models/Files.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnettest.Models
+{
+    public class Files
+    {
+        public string Stuff { get; internal set; }
+    }
+}
